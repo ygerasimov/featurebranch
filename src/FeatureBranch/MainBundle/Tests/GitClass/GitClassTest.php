6e1550ff9214e977464113af0fbc06f1cb6782ff
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-02-17 18:24:28 +0200

    First test added.

--- src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
+++ src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
@@ -3,6 +3,7 @@
 namespace FeatureBranch\MainBundle\Tests\GitClass;
 
 use FeatureBranch\MainBundle\GitClass;
+use FeatureBranch\MainBundle\CI\CIInterface;
 
 /**
  * Description of GitClassTest
@@ -27,7 +28,9 @@ class GitClassTest extends \PHPUnit_Framework_TestCase {
     $this->state_filename = '/tmp/repo_sate_' . $rand . '.yaml';
 
     $create_repo_command = 'mkdir ' . $this->origin . ' && cd ' . $this->origin
-      . ' && touch foo.txt && git init && git add . && git commit -m "init commit"';
+      . ' && touch foo.txt && git init && git add . && git commit -m "init commit"'
+      . ' && echo "foo" >> foo.txt && git checkout -b testbranch1 && git add .'
+      . ' && git commit -m "init commit to testbranch1"';
     exec($create_repo_command);
   }
 
@@ -39,8 +42,15 @@ class GitClassTest extends \PHPUnit_Framework_TestCase {
     parent::tearDown();
   }
 
-  public function testFirst() {
-    $this->assertTrue(1 > 0);
+  public function testListBranchesInStateFile() {
+    $ci = $this->getMock('\FeatureBranch\MainBundle\CI\CILogger', array('updateBranch', 'deleteBranch'));
+
+    $git = new GitClass($ci, $this->origin, $this->destination, $this->state_filename);
+    $git->checkState();
+
+    $state_file_content = file_get_contents($this->state_filename);
+
+    $this->assertContains('master', $state_file_content);
+    $this->assertContains('testbranch1', $state_file_content);
   }
-  //put your code here
 }

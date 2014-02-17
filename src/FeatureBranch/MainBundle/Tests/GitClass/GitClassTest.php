354d2632c9d6ac851c02cf5f96526d14bac2ce34
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-02-17 18:48:45 +0200

    Add test to check branches to be updated.

--- src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
+++ src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
@@ -32,6 +32,12 @@ class GitClassTest extends \PHPUnit_Framework_TestCase {
       . ' && echo "foo" >> foo.txt && git checkout -b testbranch1 && git add .'
       . ' && git commit -m "init commit to testbranch1"';
     exec($create_repo_command);
+
+    $ci = $this->getMock('\FeatureBranch\MainBundle\CI\CILogger', array('updateBranch', 'deleteBranch'));
+
+    // Initial check state.
+    $git = new GitClass($ci, $this->origin, $this->destination, $this->state_filename);
+    $git->checkState();
   }
 
   /**
@@ -42,15 +48,48 @@ class GitClassTest extends \PHPUnit_Framework_TestCase {
     parent::tearDown();
   }
 
+  /**
+   * Make sure our states file has two branches.
+   */
   public function testListBranchesInStateFile() {
-    $ci = $this->getMock('\FeatureBranch\MainBundle\CI\CILogger', array('updateBranch', 'deleteBranch'));
-
-    $git = new GitClass($ci, $this->origin, $this->destination, $this->state_filename);
-    $git->checkState();
-
     $state_file_content = file_get_contents($this->state_filename);
 
     $this->assertContains('master', $state_file_content);
     $this->assertContains('testbranch1', $state_file_content);
+  }
+
+  public function testUpdateBranches() {
+    
+    // Commit to one branch only.
+    exec($this->getCommandCommitToBranch('master'));
+
+    $ci = $this->getMock('\FeatureBranch\MainBundle\CI\CILogger', array('updateBranch', 'deleteBranch'));
+    $ci->expects($this->once())
+        ->method('updateBranch')
+        ->with('origin/master');
+    
+    $git = new GitClass($ci, $this->origin, $this->destination, $this->state_filename);
+    $git->checkState();
+
+    // Commit to two branches at the same time.
+    exec($this->getCommandCommitToBranch('master'));
+    exec($this->getCommandCommitToBranch('testbranch1'));
+    
+    $ci = $this->getMock('\FeatureBranch\MainBundle\CI\CILogger', array('updateBranch', 'deleteBranch'));
+    $ci->expects($this->at(0))
+        ->method('updateBranch')
+        ->with('origin/master');
+    $ci->expects($this->at(1))
+        ->method('updateBranch')
+        ->with('origin/testbranch1');
+    $git = new GitClass($ci, $this->origin, $this->destination, $this->state_filename);
+    $git->checkState();
+
+  }
+
+  protected function getCommandCommitToBranch($branch) {
+    return 'cd ' . $this->origin . ' && git checkout ' . $branch
+      . ' && echo "test" >> foo.txt && git add .'
+      . ' && git commit -m "commit to ' . $branch . '"';
   }
 }

9ac389a060c66d107ff1db96ddc47e2e2f759f97
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-02-17 18:04:04 +0200

    GitClass test to create test repository.

--- src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
+++ src/FeatureBranch/MainBundle/Tests/GitClass/GitClassTest.php
@@ -2,12 +2,43 @@
 
 namespace FeatureBranch\MainBundle\Tests\GitClass;
 
+use FeatureBranch\MainBundle\GitClass;
+
 /**
  * Description of GitClassTest
  *
  * @author ygerasimov
  */
 class GitClassTest extends \PHPUnit_Framework_TestCase {
+  protected $origin;
+  protected $destination;
+  protected $state_filename;
+
+  /**
+   * Creates a repository.
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $rand = rand(0, 10000);
+
+    $this->origin = '/tmp/repo_origin_' . $rand;
+    $this->destination = '/tmp/repo_destination_' . $rand;
+    $this->state_filename = '/tmp/repo_sate_' . $rand . '.yaml';
+
+    $create_repo_command = 'mkdir ' . $this->origin . ' && cd ' . $this->origin
+      . ' && touch foo.txt && git init && git add . && git commit -m "init commit"';
+    exec($create_repo_command);
+  }
+
+  /**
+   * Delete repository.
+   */
+  protected function tearDown() {
+    exec('rm -rf ' . $this->origin);
+    parent::tearDown();
+  }
+
   public function testFirst() {
     $this->assertTrue(1 > 0);
   }
